1cd1ae818d1d56f8fd3639a8b10948174f67baab
Robert Wood <rwood@Roberts-MacBook-Pro-1927.local>
2018-03-15 18:58:44 -0400

    Initial structure for runner prototype

--- webext/raptor-chrome/benchmark-relay.js
+++ webext/raptor-chrome/benchmark-relay.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // receives result from benchmark and relays onto our background runner
 
 function receiveMessage(event) {
