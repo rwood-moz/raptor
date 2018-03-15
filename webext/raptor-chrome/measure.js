e76b63858d691c1b1b45e7a28e1062a64058bfdd
Robert Wood <rwood@Roberts-MacBook-Pro-1927.local>
2018-03-15 14:47:56 -0400

    Add raptor-chrome support for the Speedometer benchmark

--- webext/raptor-chrome/measure.js
+++ webext/raptor-chrome/measure.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// content script for use with pageload tests
 var perfData = window.performance;
 var gRetryCounter = 0;
 
