0693b8081f013923e3944d10213479336bdfc621
Andrew Halberstadt <halbersa@gmail.com>
2018-03-27 13:28:22 -0400

    Implement a *very* basic ChromeRunner in mozrunner

--- vendor/mozrunner/mozrunner/base/__init__.py
+++ vendor/mozrunner/mozrunner/base/__init__.py
@@ -2,6 +2,4 @@ from __future__ import absolute_import
 
 from .runner import BaseRunner
 from .device import DeviceRunner, FennecRunner
-from .browser import GeckoRuntimeRunner
-
-__all__ = ['BaseRunner', 'DeviceRunner', 'FennecRunner', 'GeckoRuntimeRunner']
+from .browser import GeckoRuntimeRunner, BlinkRuntimeRunner
